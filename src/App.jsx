5a142a385abebfc15d2dce5fcbcb97991a0d0036
Md Ahad Hossain <hossanahad9@gmail.com>
2026-02-16 23:26:50 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Navbar from './components/Navbar';
@@ -8,30 +7,13 @@ import Footer from "./common/Footer";
 
 export default function App() {
   return (
-    <BrowserRouter>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-
-        {/* About */}
-        <Route path="/about" element={<About />} />
-
-        {/* Services */}
-       
-
-        {/* Industries */}
-       
-
-        {/* Product */}
-        
-        {/* Contact */}
-       
+    <Home/>
       </Routes>
 
       <Footer />
       {/* <FollowCursor /> */}
     </BrowserRouter>
   );
+
 }
